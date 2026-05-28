ef4fcc9017d1f37f2751dd72a8445376a0c0f4b0
mrchipmunk <66779513+Mrchipmunk@users.noreply.github.com>
2026-05-28 17:50:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,60 @@
+input.onButtonPressed(Button.A, function () {
+    if (Connected == "YES") {
+        if (LightState == "ON") {
+            radio.sendString("LAMPOFF")
+            LightState = "OFF"
+            basic.showString("OFF")
+        } else if (LightState == "OFF") {
+            radio.sendString("LAMPON")
+            LightState = "ON"
+            basic.showString("ON")
+        } else if (LightState == "CRAMODE" && CRAMODE == "ON") {
+            radio.sendString("CRAOFF")
+            CRAMODE = "OFF"
+        } else {
+            basic.showString("ERR")
+        }
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.pause(500)
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "CONNECTED") {
+        Connected = "YES"
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+let Connected = ""
+let LightState = ""
+let CRAMODE = ""
+CRAMODE = "OFF"
+let lightlevel = Math.map(input.lightLevel(), 0, 255, 0, 1023)
+LightState = "OFF"
+Connected = "NO"
+music.setBuiltInSpeakerEnabled(true)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Ringtone), music.PlaybackMode.InBackground)
+basic.showString("EVOTEST")
+radio.setGroup(1)
+radio.sendString("CONNECT")
+basic.showString("?")
+basic.forever(function () {
+	
+})
